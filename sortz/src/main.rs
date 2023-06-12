3a7a59649eb6798c91e0bfe74c61661df002ef32
BRUTALMETHOD <brutalmethod@users.noreply.github.com>
2023-06-13 07:29:35 +0800

    WIP

---
+++ sortz/src/main.rs
@@ -0,0 +1,12 @@
+struct Solution {}
+
+impl Solution {
+    pub fn quick_sort(&self, input: Vec<i32>) -> Vec<i32> {
+        input
+    }
+}
+fn main() {
+    let solution = Solution {};
+    let ans = solution.quick_sort(vec![1, 2, 3]);
+    println!("output: {:?}", ans);
+}
