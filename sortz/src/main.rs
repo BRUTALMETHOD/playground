695467e01c8ca13ce6cb0c49741669ba2a4a6a07
BRUTALMETHOD <brutalmethod@users.noreply.github.com>
2023-06-14 08:11:54 +0800

    add rng

--- sortz/src/main.rs
+++ sortz/src/main.rs
@@ -1,3 +1,4 @@
+use rand::prelude::*;
 struct Solution {}
 
 impl Solution {
@@ -5,8 +6,16 @@ impl Solution {
         input
     }
 }
+
+fn generate_random_vector(c: i32) -> Vec<i32> {
+    let mut rng = rand::thread_rng();
+    let mut r: Vec<i32> = (1..c).collect();
+    r.shuffle(&mut rng);
+    r
+}
+
 fn main() {
     let solution = Solution {};
-    let ans = solution.quick_sort(vec![1, 2, 3]);
+    let ans = solution.quick_sort(generate_random_vector(10));
     println!("output: {:?}", ans);
 }
